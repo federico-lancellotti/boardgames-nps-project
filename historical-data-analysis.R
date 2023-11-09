9a061d79ad069eb06408a9408e8ab96fb87a194b
federico-lancellotti <fedelancell@gmail.com>
2023-11-09 23:53:42 +0100

    extract unique categories, fill empty families

--- historical-data-analysis.R
+++ historical-data-analysis.R
@@ -2,10 +2,21 @@ library(roahd)
 library(bggAnalytics)
 library(dplyr)
 library(tidyr)
+library(sets)
 
 
 # Import data ----
 data <- read.csv("./data/usersRated.csv")
+
+
+# Set of categories ----
+categories <- factor(c())
+for (i in 1:length(data)) {
+  new_cat <- strsplit(data[i,]$Category, ", ")[[1]]
+  categories <- c(categories, factor(new_cat))
+}
+categories <- unique(categories)
+length(categories)  # 82
 
 
 # Preprocess ----
@@ -17,6 +28,19 @@ for (i in empty_cat.index) {
     to_remove <- c(to_remove, i)
   else
     data[i,]$Category <- data[i,]$Family
+}
+data <- data[-to_remove,]  # remove rows without Category and Family
+
+## Fill Family ----
+empty_fam.index <- which(data$Family == "")
+to_remove <- c()
+for (i in empty_fam.index) {
+  if (data[i,]$Family == "")
+    to_remove <- c(to_remove, i)
+  else {
+    categories <- strsplit(data[i,]$Category, ", ")[[1]]
+    data[i,]$Family <- categories[1]
+  }
 }
 data <- data[-to_remove,]  # remove rows without Category and Family
 
@@ -32,14 +56,19 @@ data.by_category <- data %>%
   group_by(Category) %>%
   summarise(across(where(is.numeric) & !c(ID, Year), sum))
 
+data.by_family <- data %>%
+  group_by(Family) %>%
+  summarise(across(where(is.numeric) & !c(ID, Year), sum))
+
 
 # Plots ----
 length(unique(data.by_category$Category))
+length(unique(data.family$Family))
 
 P <- ncol(data.by_category) - 1
 grid <-  seq(0, 1, length.out=P)
 
-f_data <- fData(grid, data.by_category[, -1])
+f_data <- fData(grid, log(data.by_category[, -1]))
 plot(f_data)
 
 
